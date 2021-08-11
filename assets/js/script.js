51da0921aba75250c085d635797db5bbde508f0b
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-08-11 18:31:17 -0500

    fixed clear-search bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,12 +120,12 @@ function submitSearch(event) {
         $("#movie-list").html("");
         createMovieList(currentMovieTitle);
         $("#movie-title-input").val("");
-        $("#movie-title-input").val("");
         $("header").css("display", "block")
         $(".is-fixed-top").css("display", "block")
         $(".container").css("display", "block") 
     
     }
+    $("#movie-list").html("");
     createMovieList(currentMovieTitle);
     $("#movie-title-input").val("");
     $("navbar").removeClass("box")
