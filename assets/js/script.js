d727d16bd3da405a62df8ca104f9112ef875913d
Adriana <84536824+itsmeadriana@users.noreply.github.com>
2021-08-15 18:22:06 -0500

    adding data persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-
-
 createMovieList = function (movieTitle) {
     //fetch movie info using the IMDb title/find
     fetch("https://imdb8.p.rapidapi.com/title/find?q=" + movieTitle, {
@@ -12,70 +10,12 @@ createMovieList = function (movieTitle) {
         .then(response => {
             response.json()
                 .then(function (titleData) {
+                    // saves search results to local storage
+                    var searchResultsString = JSON.stringify(titleData);
+                    saveSearchResults(movieTitle, searchResultsString);
+                    console.log("saveSearchResults")
 
-                    //Typing gibberish will return error modal
-                    if (!titleData.results) {
-                        // console.log("modal error here");
-                        let modalEl = $(".modal")
-                            .addClass("is-active");
-                    } else {
-                        //For loop to cycle through the movie results
-                        for (var i = 0; i < titleData.results.length; i++) {
-
-                            //checks to see if it has a name property, which indicates NOT a movie, so do nothing
-                            if (titleData.results[i].hasOwnProperty('name')) {
-
-                            } else {
-                                // Create the elements for the verified movie
-                                let movieImgEl = $("<img>")
-                                    .width(225)
-                                    .height(300)
-                                    .addClass("image is-centered")
-
-
-                                //checks for picture content and assigns source value
-                                if (titleData.results[i].hasOwnProperty('image')) {
-                                    movieImgEl.attr("src", titleData.results[i].image.url);
-                                } else {
-                                    movieImgEl.attr("src", "");
-                                }
-
-                                let movieDetails = $("<ul>")
-                                    .html(
-                                        titleData.results[i].title + "<p>" +
-                                        titleData.results[i].year
-                                        // + "<li> Type: " + titleData.results[i].titleType + "</li>"
-                                    )
-                                    .addClass("title is-6")
-
-                                let cardImage = $("<div>")
-                                    .width(225)
-                                    .addClass("imagewrapper")
-                                    .append(movieImgEl);
-
-                                let cardContent = $("<div>")
-                                    .width(225)
-                                    .addClass("card-content")
-                                    .append(movieDetails);
-
-                                let resultsCard = $("<div>")
-                                    .addClass("card-wrapper")
-                                    .append(cardImage, cardContent);
-
-                                let linkToSoundtrack = $("<a>")
-                                    .addClass("link")
-                                    .attr("href", "./soundtrack.html?movieID=" + titleData.results[i].id + "=movieTitle=" + movieTitle)
-                                    .append(resultsCard);
-
-                                let movieContainerEl = $("<div>")
-                                    .addClass("card")
-                                    .append(linkToSoundtrack)
-                                    .attr("id", titleData.results[i].id);
-
-                                $(".return-results").append(movieContainerEl);
-                            }
-                        }
-                    }
+                    displayImdbResults(movieTitle, titleData)
                 })
 
         })
@@ -90,17 +30,28 @@ $("#search-form").on("submit", function (event) {
     submitSearch(event);
 });
 // modal event on click
-$("#submit-button").on("click", function(event) {
+$("#submit-button").on("click", function (event) {
     submitSearch(event);
 })
 // remove modal when close-button is clicked
-$("#modal-close").on("click", function(){
+$("#modal-close").on("click", function () {
     hideModal()
 })
 // remove modal when background is clicked
-$(".modal-background").on("click", function(){
+$(".modal-background").on("click", function () {
     hideModal()
 })
+
+function saveSearchResults(movieTitle, titleData) {
+    localStorage.setItem(movieTitle, titleData);
+}
+
+function retrieveSearchResultsFromLocalStorage(movieTitle) {
+    var convertResultsFromObject = JSON.parse(localStorage.getItem(movieTitle))
+    console.log("hey")
+    return convertResultsFromObject;
+}
+
 // hides modal function
 function hideModal() {
     return $(".modal")
@@ -118,31 +69,105 @@ function submitSearch(event) {
     }
     else {
         $("#movie-list").html("");
-        createMovieList(currentMovieTitle);
+        var existingResults = function () {
+            retrieveSearchResultsFromLocalStorage(currentMovieTitle)}
+    
+        if (existingResults == null) {
+            createMovieList(currentMovieTitle);
+        }
+        else {
+            displayImdbResults(currentMovieTitle, existingResults)
+        }
+        
         $("#movie-title-input").val("");
         $("header").css("display", "block")
         $(".is-fixed-top").css("display", "block")
-        $(".container").css("display", "block") 
-    
+        $(".container").css("display", "block")
+
     }
 
+}
+
+function displayImdbResults(movieTitle, titleData) {
+    if (!titleData.results) {
+        // console.log("modal error here");
+        let modalEl = $(".modal")
+            .addClass("is-active");
+    } else {
+        //For loop to cycle through the movie results
+        for (var i = 0; i < titleData.results.length; i++) {
+
+            //checks to see if it has a name property, which indicates NOT a movie, so do nothing
+            if (titleData.results[i].hasOwnProperty('name')) {
+
+            } else {
+                // Create the elements for the verified movie
+                let movieImgEl = $("<img>")
+                    .width(225)
+                    .height(300)
+                    .addClass("image is-centered")
+
+
+                //checks for picture content and assigns source value
+                if (titleData.results[i].hasOwnProperty('image')) {
+                    movieImgEl.attr("src", titleData.results[i].image.url);
+                } else {
+                    movieImgEl.attr("src", "");
+                }
+
+                let movieDetails = $("<ul>")
+                    .html(
+                        titleData.results[i].title + "<p>" +
+                        titleData.results[i].year
+                        // + "<li> Type: " + titleData.results[i].titleType + "</li>"
+                    )
+                    .addClass("title is-6")
+
+                let cardImage = $("<div>")
+                    .width(225)
+                    .addClass("imagewrapper")
+                    .append(movieImgEl);
+
+                let cardContent = $("<div>")
+                    .width(225)
+                    .addClass("card-content")
+                    .append(movieDetails);
+
+                let resultsCard = $("<div>")
+                    .addClass("card-wrapper")
+                    .append(cardImage, cardContent);
+
+                let linkToSoundtrack = $("<a>")
+                    .addClass("link")
+                    .attr("href", "./soundtrack.html?movieID=" + titleData.results[i].id + "=movieTitle=" + movieTitle)
+                    .append(resultsCard);
+
+                let movieContainerEl = $("<div>")
+                    .addClass("card")
+                    .append(linkToSoundtrack)
+                    .attr("id", titleData.results[i].id);
+
+                $(".return-results").append(movieContainerEl);
+            }
+        }
+    }
 }
 
 // retrieve the movie if the back button was used and generate elements
-getTitle = function (){
-    var queryString = document.location.search;
-    let movieName = queryString.split("=")[1];
-    if(movieName){
-        movieName = movieName.replaceAll("%20"," ");
-        console.log(movieName);    
-        debugger;
-        createMovieList(movieName);
-        $("header").css("display", "block")
-        $(".is-fixed-top").css("display", "block")
-        $(".container").css("display", "block") 
-  
-    }
-}
+// getTitle = function (){
+//     var queryString = document.location.search;
+//     let movieName = queryString.split("=")[1];
+//     if(movieName){
+//         movieName = movieName.replaceAll("%20"," ");
+//         console.log(movieName);    
+//         debugger;
+//         createMovieList(movieName);
+//         $("header").css("display", "block")
+//         $(".is-fixed-top").css("display", "block")
+//         $(".container").css("display", "block") 
 
-getTitle();
+//     }
+// }
+
+// getTitle();
 
